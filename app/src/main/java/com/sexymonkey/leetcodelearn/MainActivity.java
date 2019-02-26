c0c0f7c048c4f05e8499cfada0f21f518c08eb9c
JerryZhu <jerryzhu@boyaa.com>
2019-02-26 14:40:08 +0800

    列表逆序

--- app/src/main/java/com/sexymonkey/leetcodelearn/MainActivity.java
+++ app/src/main/java/com/sexymonkey/leetcodelearn/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sexymonkey.leetcodelearn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mTextView = (TextView) findViewById(R.id.tv);
         int [] arr = {2,8,6,3,1,5};
 //        bubbleSort(arr);
-        sellectionSort(arr);
+//        sellectionSort(arr);
+        reverseListNode();
     }
 
     private void bubbleSort(int[] arr) {
@@ -65,5 +66,65 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, "bubbleSort: "+ i + ",");
         }
 
+    }
+
+    public void reverseListNode(){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        ListNode node = node1;
+        while (node != null){
+            Log.d(TAG,node.value+",");
+            node = node.next;
+        }
+        Log.d(TAG,"\n");
+
+        ListNode reverseListNode = reverseListNode(node1);
+        printListNode(reverseListNode);
+
+    }
+
+    public ListNode reverseListNode(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+
+        ListNode pre = null;
+        ListNode post = null;
+
+        while (head != null){
+            post = head.next;
+            head.next = pre;
+            pre = head;
+            head = post;
+        }
+        return pre;
+    }
+
+    public void printListNode(ListNode head){
+
+        ListNode current = head;
+        while (current != null){
+            Log.d(TAG,current.value +",");
+            current = current.next;
+        }
+        Log.d(TAG, "\n");
+    }
+
+    public class ListNode{
+        public ListNode next;
+        public ListNode pre;
+        public int value;
+
+        public ListNode(int value) {
+            this.value = value;
+        }
     }
 }
